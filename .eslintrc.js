b592fed8246271ddc9c711cd421631ced38b7d4d
chenzh-pc <czh533143@163.com>
2021-07-01 00:35:22 +0800

    fix: 修改一些配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@
  * @Author: zihao.chen
  * @Date: 2021-06-29 11:10:55
  * @LastEditors: czh
- * @LastEditTime: 2021-07-01 00:08:24
+ * @LastEditTime: 2021-07-01 00:19:15
  * @Description:
  */
 module.exports = {
@@ -17,11 +17,12 @@ module.exports = {
   },
   parser: '@typescript-eslint/parser',
   extends: [
-    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
     'prettier',
+    'prettier/@typescript-eslint'
   ], //定义文件继承的子规范
-  plugins: ['@typescript-eslint'], //定义了该eslint文件所依赖的插件
+  plugins: ["prettier"], //定义了该eslint文件所依赖的插件
   rules: {
     '@typescript-eslint/no-explicit-any': ['off'],
     '@typescript-eslint/ban-ts-ignore': ['off'],
--- .prettierrc.js
+++ .prettierrc.js
@@ -3,12 +3,10 @@
  * @Date: 2021-06-30 23:54:36
  * @LastEditTime: 2021-07-01 00:09:35
  * @LastEditors: czh
- * @Description: 
+ * @Description:
  */
 module.exports = {
   "eslintIntegration": true,
-  "printWidth": 150, // 每行代码长度（默认80）
-  "tabWidth": 4, // 每个tab相当于多少个空格（默认2）
   "useTabs": true, // 是否使用tab进行缩进（默认false）
   "singleQuote": true, // 使用单引号（默认false）
   "semi": false, // 声明结尾使用分号(默认true)
